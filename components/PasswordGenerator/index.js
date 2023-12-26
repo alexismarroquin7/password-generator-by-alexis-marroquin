87bc427ebc4f3f0790062d1e85d2aecc1106fd5e
alexismarroquin7 <alexismarroquin754@gmail.com>
2023-12-26 11:55:51 -0800

    adds password generator scaffolding

--- components/PasswordGenerator/index.js
+++ components/PasswordGenerator/index.js
@@ -1,5 +1,53 @@
+import styles from "./index.module.css"
+
 export const PasswordGenerator = () => {
-  return <div>
-    <p>password generator</p>
+  return <div
+    className={styles.root}
+  >
+    <p>Password Generator</p>
+
+    <label>
+      Your new password:
+      <input
+        type="text"
+      />
+    </label>
+
+    <div>
+      <button>Copy</button>
+      <button>Generate</button>
+    </div>
+
+    <label>
+      Length:
+      <button>-</button>
+      <input
+        type="text"
+      />
+      <button>+</button>
+    
+    </label>
+
+    <input 
+      type="range"
+    />
+  
+
+    <div>
+      <p>
+        Settings:
+      </p>
+      <label>Simple
+        <input
+          type="radio"
+        />
+      </label>
+      <label>Advanced
+        <input
+          type="radio"
+        />
+      </label>
+    </div>
+
   </div>
 }
